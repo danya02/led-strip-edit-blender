205e662053a4447769dbbef91624beb99db72b5f
Danya Generalov <danya@danya02.ru>
2020-03-20 09:59:37 +0300

    Adding more defined command set for Arduino aux board.

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -1,9 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN        6
-#define NUMPIXELS 554 // This has to be set accurately on both sides, otherwise updates will be out of sync. Also my setup has 576 LEDs but if this is set to 555 or higher it doesn't write any of them.
+#define LEDPIN		6
+#define PSUPIN		12
+#define NUMPIXELS	554 // This has to be set accurately on both sides, otherwise updates will be out of sync. Also my setup has 576 LEDs but if this is set to 555 or higher it doesn't write any of them.
 
-#define SERIAL_BAUDRATE 250000
+#define SERIAL_BAUDRATE	250000
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN);
 
@@ -23,36 +24,32 @@ byte readOneByte() {
   return (byte)ans;
 }
 
-byte last_color[] = {0,0,0};
+/* Accepted commands are ("?" symbol stands for "don't care" byte):
+	"123:\x00\xff\x80\n": Set pixel 123 to 0x0 red, 0xff green and 0x80 blue
+	"-1:?\n": Display loaded values on LED strip (takes significant time)
+	"0:\x80\n": If the value after the colon is not 0, then enable the LED power supply.
+After each command is completed, the string "OK\n" is sent. When sent, it means the board is
+ready for more commands.
+*/
 
 void loop() {
-restartLoop:
-  for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-
-//    Serial.print("Reading pix ");
-//    Serial.println(i);
     while(!Serial.available()){}
     int index = Serial.parseInt(SKIP_ALL);
     Serial.read();
-    int r,g,b;
-    r=Serial.parseInt(SKIP_ALL);
-    Serial.read();
-    g=Serial.parseInt(SKIP_ALL);
-    Serial.read();
-    b=Serial.parseInt(SKIP_ALL);
+    if(index==0){
+        int val = Serial.read();
+        digitalWrite(PSUPIN, val);
+    } else if (index==-1) {
+        pixels.show();
+    } else {
+        int r,g,b;
+        r=Serial.read();
+        g=Serial.read();
+        b=Serial.read();
+	pixels.setPixelColor(index, pixels.Color(r,g,b));
+    }
+    Serial.print("OK\n");
+    Serial.flush();
     Serial.readStringUntil('\n');
-  
-    Serial.print(index);
-    Serial.print(" ==> ");
-    Serial.print(r);
-    Serial.print(" ");
-    Serial.print(g);
-    Serial.print(" ");
-    Serial.println(b);
-
-
-    pixels.setPixelColor(index, pixels.Color(r,g,b));
-
-    if(index==-1){pixels.show();}
   }
 }
